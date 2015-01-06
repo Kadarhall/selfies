f0106107b5418a74164805c6031d735baf26f785
Kadar Hall <khallks@gmail.com>
2015-01-06 10:53:52 -0500

    functionality created

--- app/controllers/selfies_controller.rb
+++ app/controllers/selfies_controller.rb
@@ -9,8 +9,23 @@ class SelfiesController < ApplicationController
 		@selfies = Selfie.find(params[:id])
 	end
 
-	
+	def new
+		@selfie = Selfie.new
+	end
 
+	def create
+		@selfie = Selfie.new(task_params)
+		if @selfie.save
+			redirect_to '/'
+		else
+			render :new
+		end
+	end
 
+	private
+
+	def task_params
+		params.require(:selfie).permit(:body)
+	end
 
 end
